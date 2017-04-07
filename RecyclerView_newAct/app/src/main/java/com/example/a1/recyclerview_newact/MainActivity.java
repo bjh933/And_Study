86568c705c92ddd726e58f170bf2de0637bcfeec
bjh <a1@1ui-MacBook-Pro.local>
2017-04-07 20:45:34 +0900

    reset

---
+++ RecyclerView_newAct/app/src/main/java/com/example/a1/recyclerview_newact/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a1.recyclerview_newact;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
